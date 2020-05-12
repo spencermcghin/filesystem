433fd1beeb32702b145758a71645ba7f24da2f92
spencermcghin <spencer.mcghin@gmail.com>
2020-05-12 12:53:51 -0700

    Added cd command
    Signed-off-by: spencermcghin <spencer.mcghin@gmail.com>

--- src/com/rtjvm/scala/oop/commands/Command.scala
+++ src/com/rtjvm/scala/oop/commands/Command.scala
@@ -14,6 +14,7 @@ object Command {
   val LS = "ls"
   val PWD = "pwd"
   val TOUCH = "touch"
+  val CD = "cd"
 
   def emptyCommand: Command = new Command {
     override def apply(state: State): State = state
@@ -38,6 +39,9 @@ object Command {
     } else if (TOUCH.equals(tokens(0))) {
       if (tokens.length < 2) incompleteCommand(TOUCH)
       else new Touch(tokens(1))
+    } else if (CD.equals(tokens(0))) {
+      if (tokens.length < 2) incompleteCommand(CD)
+      else new Cd(tokens(1))
     }
     else new UnknownCommand
   }
